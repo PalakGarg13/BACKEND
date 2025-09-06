0d717e299500c2037ee131dd4a17ca933ac81306
PalakGarg13 <156164547+PalakGarg13@users.noreply.github.com>
2025-09-06 22:26:07 +0530

    Backend

---
+++ 14mongoimplementation.js
@@ -0,0 +1,44 @@
+//mongo installation
+//mongodb connection
+//schema
+//model
+//crud
+//npm i mongoose - helps in conversation be node server and mongodb server
+//ORM - object relational mapping
+//ODM - object document mapping
+
+
+const express = require('express');
+const app = express();
+const userModel = require('./15usermodel');
+app.get('/',(req,res)=>{
+res.send("hey");
+})
+app.get('/create',async (req,res)=>{
+// userModel.create({ //yeh asynchronous code hai toh yeh baadmein chlega
+//     name: "Palak",
+//     username : "Palak",
+//     email : "palak11@gmail.com"
+// })
+// console.log("hey"); //it is synchronous toh yeh pehle chlega
+
+//bt if we want ki async wala pehle chle toh we did
+let createduser = await userModel.create({ //if we use await then it is imp to write async in parent function
+    name:"Palak",
+    username : "palak",
+    email : "palak11@gmail.com"
+})
+res.send(createduser);
+})
+
+app.get('/update', async (req,res)=>{
+// userModel.findOneAndUpdate(findone,update, {new: true}) //syntax
+let updateduser =  await userModel.findOneAndUpdate({username:"Palak"}, {name : "Palak garg"},{new: true})
+res.send(updateduser);
+})
+
+
+// app.get('/',(req,res)=>{
+// res.send("hey");
+// })
+app.listen(3000);
---
+++ 15usermodel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+const userSchema = mongoose.Schema({
+   name:String,
+   username:String,
+   email:String
+})
+//create a model for create read update
+module.exports = mongoose.model("user",userSchema);
